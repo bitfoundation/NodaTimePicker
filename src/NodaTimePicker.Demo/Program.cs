546d398fdd8e83c20c2c0521c234893dc4235087
Yaser Moradi <ysmoradi@outlook.com>
2020-08-17 13:44:20 +0430

    persian solar sample

--- src/NodaTimePicker.Demo/Program.cs
+++ src/NodaTimePicker.Demo/Program.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Globalization;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NodaTimePicker.Demo
 {
-	public class Program
+    public class Program
 	{
 		public static async Task Main(string[] args)
 		{
+			CultureInfo Fa = new CultureInfo("fa-IR");
+
+			Fa.DateTimeFormat.MonthNames = Fa.DateTimeFormat.AbbreviatedMonthGenitiveNames = Fa.DateTimeFormat.MonthGenitiveNames = Fa.DateTimeFormat.AbbreviatedMonthNames = new[] { "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند", "" };
+
+			CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture = Fa;
+			
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("app");
 
